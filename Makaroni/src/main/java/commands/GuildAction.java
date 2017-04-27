e80170482dc72990800d21484e9444bb8adb07b6
Marcus Terry <Makartws2@Hotmail.com>
2017-04-27 16:52:17 -0500

    Update

--- Makaroni/src/main/java/commands/GuildAction.java
+++
@@ -1,7 +0,0 @@
-package commands;
-import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
-
-abstract class GuildAction {
-	public abstract String getCommand();
-	public abstract void run(GuildMessageReceivedEvent event);
-}
