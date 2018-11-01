ccb5601464b52a752a774d466ac4ec11a6ae6f44
Marcus Terry <Makartws2@Hotmail.com>
2018-10-31 23:43:33 -0500

    UX Adjustments

--- src/main/java/utils/DiscordID.java
+++ src/main/java/utils/DiscordID.java
@@ -1,9 +1,5 @@
 package utils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class DiscordID {
 	public static final long PROGRAMMING = 176944717987053568L;
 	public static final long GAMING = 91676956352868352L;
@@ -14,13 +10,4 @@ public class DiscordID {
 	public static final String KYSIS = "175172897155973120";
 	public static final String KAGA = "173302124656984064";
 	public static final String VENUS = "374995210591272960";
-	public static HashMap<String, ArrayList<String>> EXCLUSIONS;
-	static {
-		EXCLUSIONS = new HashMap<String, ArrayList<String>>();
-		EXCLUSIONS.put(LIN, new ArrayList<>(Arrays.asList(BECCA)));
-		EXCLUSIONS.put(BECCA, new ArrayList<>(Arrays.asList(LIN)));
-		EXCLUSIONS.put(KYSIS, new ArrayList<>(Arrays.asList(VENUS)));
-		EXCLUSIONS.put(KAGA, new ArrayList<>(Arrays.asList(VENUS)));
-		EXCLUSIONS.put(VENUS, new ArrayList<>(Arrays.asList(KYSIS, KAGA)));
-	}
 }
--- src/main/java/utils/SantaManager.java
+++ src/main/java/utils/SantaManager.java
@@ -7,136 +7,157 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SantaManager implements Serializable {
-	private static final long serialVersionUID = 6144114037574556203L;
-	private Map<String, Santa> santas;
-	private Map<String, String> selectedPairs;
-	private boolean gameStarted;
+    private static final long serialVersionUID = 6144114037574556203L;
+    private Map<String, Santa> santas;
+    private Map<String, String> selectedPairs;
+    private boolean gameStarted;
 
-	public SantaManager() {
-		santas = new HashMap<String, Santa>();
-		selectedPairs = new HashMap<String, String>();
-		gameStarted = false;
-	}
+    public SantaManager() {
+        santas = new HashMap<String, Santa>();
+        selectedPairs = new HashMap<String, String>();
+        gameStarted = false;
+    }
 
-	public boolean add(Santa santa) {
-		boolean ret = !santas.containsKey(santa.getDiscordID());
-		santas.put(santa.getDiscordID(), santa);
-		updateSantas();
-		return ret;
-	}
+    public boolean add(Santa santa) {
+        boolean ret = !santas.containsKey(santa.getDiscordID());
+        santas.put(santa.getDiscordID(), santa);
+        updateSantas();
+        return ret;
+    }
 
-	public boolean remove(String discordID) {
-		boolean ret = santas.containsKey(discordID);
-		santas.remove(discordID);
-		updateSantas();
-		return ret;
-	}
-	
-	public int size() {
-		return santas.size();
-	}
-	
-	public void setReceiver(String santa, String receiver) {
-		selectedPairs.put(santa, receiver);
-		updateSantas();
-	}
-	
-	public String getReceiver(String santa) {
-		if(!gameStarted)
-			return null;
-		
-		return selectedPairs.get(santa);
-	}
-	
-	public String getSanta(String receiver) {
-		if(!gameStarted)
-			return null;
+    public boolean remove(String discordID) {
+        boolean ret = santas.containsKey(discordID);
+        santas.remove(discordID);
+        updateSantas();
+        return ret;
+    }
 
-		for (Map.Entry<String, String> entry : selectedPairs.entrySet()) {
-		    String s = entry.getKey();
-		    String r = entry.getValue();
-		    if(r.equals(receiver))
-		    	return s;
-		}
-		
-		return null;
-	}
-	
-	public boolean started() {
-		return this.gameStarted;
-	}
-	
-	public void start() {
-		this.gameStarted = true;
-		updateSantas();
-	}
-	
-	public void reset() {
-		this.santas = new HashMap<String, Santa>();
-		this.selectedPairs = new HashMap<String, String>();
-		this.gameStarted = false;
-		updateSantas();
-	}
+    public int size() {
+        return santas.size();
+    }
 
-	public ArrayList<Santa> getAll() {
-		ArrayList<Santa> ret = new ArrayList<Santa>(santas.values());
-		do
-			Collections.shuffle(ret);
-		while(!checkExclusions(ret));
+    public void setReceiver(String santa, String receiver) {
+        selectedPairs.put(santa, receiver);
+        updateSantas();
+    }
 
-		return ret;
-	}
-	
-	public String toString() {
-		StringBuilder str = new StringBuilder();
-		
-		santas.forEach((k, s) -> {
-			str.append(s.toString());
-			str.append("\n\n");
-		});
-		
-		return str.toString();
-	}
+    public String getReceiver(String santa) {
+        if (!gameStarted)
+            return null;
 
-	public static boolean checkExclusions(ArrayList<Santa> s) {
-		for (int i = 0; i < s.size(); i++) {
-			int j = i == s.size() - 1 ? 0 : i + 1;
-			if(DiscordID.EXCLUSIONS.get(s.get(i).getDiscordID()).contains(s.get(j).getDiscordID()))
-				return false;
-		}
-		return true;
-	}
-	
-	public static SantaManager loadSantas() {
-		SantaManager s;
+        return selectedPairs.get(santa);
+    }
 
-		try {
-			FileInputStream fis = new FileInputStream("santas.dat");
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			s = (SantaManager) ois.readObject();
-			ois.close();
-			fis.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new SantaManager();
-		}
-		
-		return s;
-	}
-	
-	private boolean updateSantas() {
-		try {
-			new File("santas.dat").delete();
-			FileOutputStream fos = new FileOutputStream("santas.dat");
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(this);
-			oos.close();
-			fos.close();
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public String getSanta(String receiver) {
+        if (!gameStarted)
+            return null;
 
-		return false;
-	}
+        for (Map.Entry<String, String> entry : selectedPairs.entrySet()) {
+            String s = entry.getKey();
+            String r = entry.getValue();
+            if (r.equals(receiver))
+                return s;
+        }
+
+        return null;
+    }
+
+    public boolean started() {
+        return this.gameStarted;
+    }
+
+    public void start() {
+        this.gameStarted = true;
+        updateSantas();
+    }
+
+    public void reset() {
+        this.santas = new HashMap<String, Santa>();
+        this.selectedPairs = new HashMap<String, String>();
+        this.gameStarted = false;
+        updateSantas();
+    }
+
+    public ArrayList<Santa> getAll() {
+        ArrayList<Santa> ret = new ArrayList<Santa>(santas.values());
+        do
+            Collections.shuffle(ret);
+        while (!checkExclusions(ret));
+
+        return ret;
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        santas.forEach((k, s) -> {
+            str.append(s.toString());
+            str.append("\n\n");
+        });
+
+        return str.toString();
+    }
+
+    public static boolean checkExclusions(ArrayList<Santa> s) {
+        for (int i = 0; i < s.size(); i++) {
+            int j = i == s.size() - 1 ? 0 : i + 1;
+
+            String id1 = s.get(i).getDiscordID();
+            String id2 = s.get(j).getDiscordID();
+
+            switch (id1) {
+                case DiscordID.LIN:
+                    if (id2.equals(DiscordID.BECCA))
+                        return false;
+                case DiscordID.BECCA:
+                    if (id2.equals(DiscordID.LIN))
+                        return false;
+                case DiscordID.KYSIS:
+                    if (id2.equals(DiscordID.VENUS))
+                        return false;
+                case DiscordID.KAGA:
+                    if (id2.equals(DiscordID.VENUS))
+                    return false;
+                case DiscordID.VENUS:
+                    if(id2.equals(DiscordID.KAGA))
+                        return false;
+                    if(id2.equals(DiscordID.KYSIS))
+                        return false;
+            }
+        }
+        return true;
+    }
+
+    public static SantaManager loadSantas() {
+        SantaManager s;
+
+        try {
+            FileInputStream fis = new FileInputStream("santas.dat");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            s = (SantaManager) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new SantaManager();
+        }
+
+        return s;
+    }
+
+    private boolean updateSantas() {
+        try {
+            new File("santas.dat").delete();
+            FileOutputStream fos = new FileOutputStream("santas.dat");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+            oos.close();
+            fos.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
