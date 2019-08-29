fcc2db25bf7dd9d943bf74a658c9cd002a510bfb
Marcus Terry <Makartws2@Hotmail.com>
2019-08-29 03:08:11 -0500

    Add !deleteall command

--- src/main/java/commands/AdminCommandListener.java
+++ src/main/java/commands/AdminCommandListener.java
@@ -18,203 +18,227 @@ import javax.script.ScriptEngineManager;
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AdminCommandListener extends ListenerAdapter {
-	private final Map<String, GuildAction> commands;
-	private final Map<String, PrivateAction> privateCommands;
+    private final Map<String, GuildAction> commands;
+    private final Map<String, PrivateAction> privateCommands;
 
-	public AdminCommandListener() {
-		commands = new HashMap<>();
-		privateCommands = new HashMap<>();
-		addCommands();
-	}
+    public AdminCommandListener() {
+        commands = new HashMap<>();
+        privateCommands = new HashMap<>();
+        addCommands();
+    }
 
-	@Override
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-		if (!event.getAuthor().getId().equals(DiscordID.ADMIN_ID))
-			return;
-		String command = event.getMessage().getContentRaw().split(" ", 2)[0].toLowerCase();
-		if (commands.containsKey(command))
-			commands.get(command).run(event);
-	}
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        if (!event.getAuthor().getId().equals(DiscordID.ADMIN_ID))
+            return;
+        String command = event.getMessage().getContentRaw().split(" ", 2)[0].toLowerCase();
+        if (commands.containsKey(command))
+            commands.get(command).run(event);
+    }
 
-	@Override
-	public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
-		if (!event.getAuthor().getId().equals(DiscordID.ADMIN_ID))
-			return;
-		String command = event.getMessage().getContentRaw().split(" ", 2)[0].toLowerCase();
-		if (privateCommands.containsKey(command))
-			privateCommands.get(command).run(event);
-	}
+    @Override
+    public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
+        if (!event.getAuthor().getId().equals(DiscordID.ADMIN_ID))
+            return;
+        String command = event.getMessage().getContentRaw().split(" ", 2)[0].toLowerCase();
+        if (privateCommands.containsKey(command))
+            privateCommands.get(command).run(event);
+    }
 
-	@Override
-	public void onResume(ResumedEvent event) {
-		setGame(event.getJDA(), Constants.DEFAULT_GAME);
-	}
+    @Override
+    public void onResume(ResumedEvent event) {
+        setGame(event.getJDA(), Constants.DEFAULT_GAME);
+    }
 
-	@Override
-	public void onReconnect(ReconnectedEvent event) {
-		setGame(event.getJDA(), Constants.DEFAULT_GAME);
-	}
+    @Override
+    public void onReconnect(ReconnectedEvent event) {
+        setGame(event.getJDA(), Constants.DEFAULT_GAME);
+    }
 
-	private void setGame(JDA jda, String game) {
-		jda.getPresence().setGame(Game.playing(game));
-	}
+    private void setGame(JDA jda, String game) {
+        jda.getPresence().setGame(Game.playing(game));
+    }
 
-	private void addCommands() {
-		addSetGameCommand();
-		addShutdownCommand();
-		addDeleteCommand();
-		addEvalCommand();
-		addRemoveCommand();
-		addDynamicCommand();
-	}
+    private void addCommands() {
+        addSetGameCommand();
+        addShutdownCommand();
+        addDeleteCommand();
+        addEvalCommand();
+        addRemoveCommand();
+        addDynamicCommand();
+        addDeleteAllCommand();
+    }
 
-	private PrivateAction addSetGameCommand() {
-		PrivateAction action = new PrivateAction() {
-			public String getCommand() {
-				return "setgame";
-			}
+    private PrivateAction addDeleteAllCommand() {
+        PrivateAction action = new PrivateAction() {
+            public String getCommand() {
+                return "deleteall";
+            }
 
-			public void run(PrivateMessageReceivedEvent event) {
-				String[] command = event.getMessage().getContentRaw().split(" ", 2);
-				if (command.length != 2)
-					return;
+            public void run(PrivateMessageReceivedEvent event) {
+                String[] command = event.getMessage().getContentRaw().split(" ", 2);
+                if (command.length != 2)
+                    return;
 
-				setGame(event.getJDA(), command[1]);
-			}
-		};
-		privateCommands.put(Constants.PREFIX + action.getCommand(), action);
-		return action;
-	}
+                TextChannel chan = event.getJDA().getTextChannelById(command[1]);
+                for (Message message : chan.getIterableHistory().stream()
+                        .limit(1000)
+                        .filter(m -> !m.isPinned())
+                        .collect(Collectors.toList()))
+                    message.delete().queue();
+            }
+        };
+        privateCommands.put(Constants.PREFIX + action.getCommand(), action);
+        return action;
+    }
 
-	private GuildAction addShutdownCommand() {
-		GuildAction action = new GuildAction() {
-			public String getCommand() {
-				return "shutdown";
-			}
+    private PrivateAction addSetGameCommand() {
+        PrivateAction action = new PrivateAction() {
+            public String getCommand() {
+                return "setgame";
+            }
 
-			public void run(GuildMessageReceivedEvent event) {
-				event.getJDA().shutdown();
-				System.exit(0);
-			}
-		};
-		commands.put(Constants.PREFIX + action.getCommand(), action);
-		return action;
-	}
+            public void run(PrivateMessageReceivedEvent event) {
+                String[] command = event.getMessage().getContentRaw().split(" ", 2);
+                if (command.length != 2)
+                    return;
 
-	private GuildAction addDeleteCommand() {
-		GuildAction action = new GuildAction() {
-			public String getCommand() {
-				return "deletemsg";
-			}
+                setGame(event.getJDA(), command[1]);
+            }
+        };
+        privateCommands.put(Constants.PREFIX + action.getCommand(), action);
+        return action;
+    }
 
-			public void run(GuildMessageReceivedEvent event) {
-				String[] command = event.getMessage().getContentRaw().split(" ", 4);
-				if (command.length == 4) {
-					String strChan = command[1];
-					String strFrom = command[2];
-					String strTo = command[3];
-					TextChannel chan = event.getGuild().getTextChannelById(strChan);
-					Message start = chan.getMessageById(strFrom).complete();
-					Message end = chan.getMessageById(strTo).complete();
-					for (Message message : chan.getIterableHistory()) {
-						if (message.getCreationTime().isAfter(start.getCreationTime()) && message.getCreationTime().isBefore(end.getCreationTime())) {
-							System.out.println(message.getAuthor().getName() + ": " + message.getContentRaw());
-							message.delete().queue();
-						}
-					}
-				}
-			}
-		};
-		commands.put(Constants.PREFIX + action.getCommand(), action);
-		return action;
-	}
-	
-	private GuildAction addEvalCommand() {
-		GuildAction action = new GuildAction() {
-			public String getCommand() {
-				return "eval";
-			}
+    private GuildAction addShutdownCommand() {
+        GuildAction action = new GuildAction() {
+            public String getCommand() {
+                return "shutdown";
+            }
 
-			public void run(GuildMessageReceivedEvent event) {
-				String[] command = event.getMessage().getContentRaw().split(" ", 2);
-				if (command.length != 2) 
-					return;
-				
-				String output = "```";
-				EmbedBuilder msg = new EmbedBuilder();
-				msg.setAuthor(event.getAuthor().getName(), null, event.getAuthor().getEffectiveAvatarUrl());
-	    		msg.setTitle("Status", null);
+            public void run(GuildMessageReceivedEvent event) {
+                event.getJDA().shutdown();
+                System.exit(0);
+            }
+        };
+        commands.put(Constants.PREFIX + action.getCommand(), action);
+        return action;
+    }
 
-				ScriptEngine se = new ScriptEngineManager().getEngineByName("Nashorn");
-		        se.put("event", event);
-		        se.put("jda", event.getJDA());
-		        se.put("guild", event.getGuild());
-		        se.put("channel", event.getChannel());
-		        try {
-		        	output += (String) se.eval(command[1]);
-		    		msg.setColor(Color.GREEN);
-		    		msg.setDescription("Evaluated successfully.");
-		        } 
-		        catch(Exception e) {
-		        	output += e;
-		    		msg.setColor(Color.RED);
-		    		msg.setDescription("An exception was thrown.");
-		        }
-		        
-		        output += "```";
-	    		msg.addField("Output", output, false);
-	        	event.getChannel().sendMessage(msg.build()).queue();
-			}
-		};
-		commands.put(Constants.PREFIX + action.getCommand(), action);
-		return action;
-	}
+    private GuildAction addDeleteCommand() {
+        GuildAction action = new GuildAction() {
+            public String getCommand() {
+                return "deletemsg";
+            }
 
-	private GuildAction addRemoveCommand() {
-		GuildAction action = new GuildAction() {
-			public String getCommand() {
-				return "rem";
-			}
+            public void run(GuildMessageReceivedEvent event) {
+                String[] command = event.getMessage().getContentRaw().split(" ", 4);
+                if (command.length == 4) {
+                    String strChan = command[1];
+                    String strFrom = command[2];
+                    String strTo = command[3];
+                    TextChannel chan = event.getGuild().getTextChannelById(strChan);
+                    Message start = chan.getMessageById(strFrom).complete();
+                    Message end = chan.getMessageById(strTo).complete();
+                    for (Message message : chan.getIterableHistory()) {
+                        if (message.getCreationTime().isAfter(start.getCreationTime()) && message.getCreationTime().isBefore(end.getCreationTime())) {
+                            System.out.println(message.getAuthor().getName() + ": " + message.getContentRaw());
+                            message.delete().queue();
+                        }
+                    }
+                }
+            }
+        };
+        commands.put(Constants.PREFIX + action.getCommand(), action);
+        return action;
+    }
 
-			public void run(GuildMessageReceivedEvent event) {
-				String[] command = event.getMessage().getContentRaw().split(" ", 2);
-				if (command.length == 2)
-					commands.remove(command[1]);
-			}
-		};
-		commands.put(Constants.PREFIX + action.getCommand(), action);
-		return action;
-	}
+    private GuildAction addEvalCommand() {
+        GuildAction action = new GuildAction() {
+            public String getCommand() {
+                return "eval";
+            }
 
-	private GuildAction addDynamicCommand() {
-		GuildAction action = new GuildAction() {
-			public String getCommand() {
-				return "add";
-			}
+            public void run(GuildMessageReceivedEvent event) {
+                String[] command = event.getMessage().getContentRaw().split(" ", 2);
+                if (command.length != 2)
+                    return;
 
-			public void run(GuildMessageReceivedEvent event) {
-				String[] command = event.getMessage().getContentRaw().split(" ", 3);
-				if (command.length == 3) {
-					GuildAction newCommand = newDynamicCommand(command[1], command[2]);
-					commands.put(newCommand.getCommand(), newCommand);
-				}
-			}
-		};
-		commands.put(Constants.PREFIX + action.getCommand(), action);
-		return action;
-	}
+                String output = "```";
+                EmbedBuilder msg = new EmbedBuilder();
+                msg.setAuthor(event.getAuthor().getName(), null, event.getAuthor().getEffectiveAvatarUrl());
+                msg.setTitle("Status", null);
 
-	private GuildAction newDynamicCommand(String command, String response) {
-		return new GuildAction() {
-			public String getCommand() {
-				return Constants.PREFIX + command;
-			}
+                ScriptEngine se = new ScriptEngineManager().getEngineByName("Nashorn");
+                se.put("event", event);
+                se.put("jda", event.getJDA());
+                se.put("guild", event.getGuild());
+                se.put("channel", event.getChannel());
+                try {
+                    output += (String) se.eval(command[1]);
+                    msg.setColor(Color.GREEN);
+                    msg.setDescription("Evaluated successfully.");
+                } catch (Exception e) {
+                    output += e;
+                    msg.setColor(Color.RED);
+                    msg.setDescription("An exception was thrown.");
+                }
 
-			public void run(GuildMessageReceivedEvent event) {
-				event.getChannel().sendMessage(response).queue();
-			}
-		};
-	}
+                output += "```";
+                msg.addField("Output", output, false);
+                event.getChannel().sendMessage(msg.build()).queue();
+            }
+        };
+        commands.put(Constants.PREFIX + action.getCommand(), action);
+        return action;
+    }
+
+    private GuildAction addRemoveCommand() {
+        GuildAction action = new GuildAction() {
+            public String getCommand() {
+                return "rem";
+            }
+
+            public void run(GuildMessageReceivedEvent event) {
+                String[] command = event.getMessage().getContentRaw().split(" ", 2);
+                if (command.length == 2)
+                    commands.remove(command[1]);
+            }
+        };
+        commands.put(Constants.PREFIX + action.getCommand(), action);
+        return action;
+    }
+
+    private GuildAction addDynamicCommand() {
+        GuildAction action = new GuildAction() {
+            public String getCommand() {
+                return "add";
+            }
+
+            public void run(GuildMessageReceivedEvent event) {
+                String[] command = event.getMessage().getContentRaw().split(" ", 3);
+                if (command.length == 3) {
+                    GuildAction newCommand = newDynamicCommand(command[1], command[2]);
+                    commands.put(newCommand.getCommand(), newCommand);
+                }
+            }
+        };
+        commands.put(Constants.PREFIX + action.getCommand(), action);
+        return action;
+    }
+
+    private GuildAction newDynamicCommand(String command, String response) {
+        return new GuildAction() {
+            public String getCommand() {
+                return Constants.PREFIX + command;
+            }
+
+            public void run(GuildMessageReceivedEvent event) {
+                event.getChannel().sendMessage(response).queue();
+            }
+        };
+    }
 }
