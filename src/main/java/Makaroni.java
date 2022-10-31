333b3a464123d43b3aa0b96b9046eef297c2ccef
Makar8000 <Makar8000@users.noreply.github.com>
2022-10-31 09:46:44 -0500

    update JDA to 4.4.0

--- src/main/java/Makaroni.java
+++ src/main/java/Makaroni.java
@@ -2,11 +2,16 @@ import commands.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import utils.TokenManager;
+
+import java.util.ArrayList;
 
 public class Makaroni {
     public static void main(String[] args) throws Exception {
-        JDA jda = new JDABuilder().setToken(TokenManager.getDiscordTok()).build().awaitReady();
+        JDA jda = JDABuilder.createDefault(TokenManager.getDiscordTok())
+                .enableIntents(getIntents())
+                .build().awaitReady();
         jda.addEventListener(new GuildCommandListener());
         jda.addEventListener(new AdminCommandListener());
         //jda.addEventListener(new NicknameChangeListener());
@@ -15,5 +20,16 @@ public class Makaroni {
         jda.addEventListener(new XIVCommandListener());
         jda.addEventListener(new SecretSantaCommandListener());
         jda.getPresence().setActivity(Activity.playing("with kittens"));
+    }
+
+    private static ArrayList<GatewayIntent> getIntents() {
+        // Add non-default intents
+        ArrayList<GatewayIntent> intents = new ArrayList<>();
+        intents.add(GatewayIntent.GUILD_MEMBERS);
+        intents.add(GatewayIntent.GUILD_PRESENCES);
+        intents.add(GatewayIntent.GUILD_WEBHOOKS);
+        intents.add(GatewayIntent.GUILD_MESSAGE_TYPING);
+        intents.add(GatewayIntent.DIRECT_MESSAGE_TYPING);
+        return intents;
     }
 }
